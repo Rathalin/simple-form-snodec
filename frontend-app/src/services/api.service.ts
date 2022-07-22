28518cad553b62693173a41b5b9ca74a37828fe1
Rathalin <danielflo1997@hotmail.com>
2022-07-22 21:24:08 +0200

    Add api service class to frontend app

---
+++ frontend-app/src/services/api.service.ts
@@ -0,0 +1,29 @@
+export interface User {
+    email: string
+    created_at: string
+}
+
+class ApiService {
+    private readonly API_URL: string = 'http://localhost:8080/api'
+
+
+    async login(): Promise<boolean> {
+        const response = await fetch(`${this.API_URL}/login`, {
+            method: 'post',
+            body: JSON.stringify({
+                email: 'daniel@flockert.at',
+                password: 'rathalin',
+            })
+        })
+        return response.status === 200
+    }
+
+    async getUsers(): Promise<User[]> {
+        const response = await fetch(`${this.API_URL}/users`, {
+            method: 'get',
+        })
+        return await response.json() as User[]
+    }
+}
+
+export const apiService = new ApiService();
