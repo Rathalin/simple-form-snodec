9de8a39c19fd6b76af85db23d62314adf7bc74e5
PhilToolan <philtoolan@gmail.com>
2022-09-23 13:26:01 +0200

    Create Comments and Threads

---
+++ frontend-app/src/components/TopicInput.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    
+    const props = defineProps<{
+      inputLabel?: string
+      inputPlaceholder?: string
+      buttonLabel: string
+    }>()
+    
+    const emits = defineEmits<{
+      (e: 'submit-input', input: string): void
+    }>()
+    
+    const inputText = ref('')
+    const inputFocused = ref(false)
+    
+    function submitInput(): void {
+      emits('submit-input', inputText.value)
+      inputText.value = ''
+    }
+    </script>
+    
+    <template>
+      <div class="input-wrapper flex-col">
+        <label v-if="props.inputLabel" for="new-topic">{{ props.inputLabel }}</label>
+        <div class="flex-row">
+          <input v-model="inputText" @focus="inputFocused = true" @blur="inputFocused = false" @keydown.enter="submitInput"
+            :placeholder="props.inputPlaceholder" type="text" name="new-topic" id="new-topic" autocomplete="off"
+            class="flex-grow">
+            <input >
+          <button v-if="inputFocused || inputText" :disabled="inputText.length === 0" @click="submitInput">
+            {{ props.buttonLabel }}
+          </button>
+        </div>
+      </div>
+    </template>
+    
+    <style scoped lang="scss">
+    .input-wrapper {
+      margin-top: 1em;
+    
+      & > div {
+        gap: 0.4rem;
+      }
+    }
+    
+    input,
+    button {
+      box-shadow: var(--shadow-second);
+    }
+    </style>
+    
